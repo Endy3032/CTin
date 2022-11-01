92eab74dc0bf410e845465ec80c8a6fe611ce81f
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-11-01 21:41:15 +0700

    update indentation

--- TDN/GK/D2/B3/BAI3.cpp
+++ TDN/GK/D2/B3/BAI3.cpp
@@ -9,18 +9,18 @@ int main() {
   freopen("BAI3.out", "w", stdout);
   cin >> n;
 
-	vector<ll> a(n + 1);
-	a[0] = 0;
+  vector<ll> a(n + 1);
+  a[0] = 0;
 
-	for (ll i = 1; i <= n; i++) {
-		cin >> a[i];
-		a[i] += a[i - 1];
-	}
+  for (ll i = 1; i <= n; i++) {
+    cin >> a[i];
+    a[i] += a[i - 1];
+  }
 
   for (ll ln = 2; ln <= n / 2; ln++) {
-		for (ll x = ln * 2; x <= n; x++) {
-			m = max(m, a[x] - a[x - ln * 2]);
-		}
+    for (ll x = ln * 2; x <= n; x++) {
+      m = max(m, a[x] - a[x - ln * 2]);
+    }
   }
   cout << m;
 }
--- TDN/GK/D2/B4/BAI4.cpp
+++ TDN/GK/D2/B4/BAI4.cpp
@@ -5,11 +5,11 @@ typedef long long ll;
 ll n, m = 0, pwr, prc;
 
 int main() {
-	freopen("BAI4.INP", "r", stdin);
-	freopen("BAI4.OUT", "w", stdout);
+  freopen("BAI4.INP", "r", stdin);
+  freopen("BAI4.OUT", "w", stdout);
 
-	cin >> n;
-	vector<ll> c(n);
+  cin >> n;
+  vector<ll> c(n);
   for (ll i = 0; i < n; i++) cin >> c[i];
   sort(c.begin(), c.end());
 
@@ -22,11 +22,11 @@ int main() {
   sort(ac.begin(), ac.end());
 
   for (ll x = 0; x < c.size(); x++) {
-		for (ll y = 0; y < ac.size(); y++) {
-			if (ac[y].second < c[x]) continue;
-			m += ac[y].first;
-			break;
-		}
-	}
-	cout << m << endl;
+    for (ll y = 0; y < ac.size(); y++) {
+      if (ac[y].second < c[x]) continue;
+      m += ac[y].first;
+      break;
+    }
+  }
+  cout << m << endl;
 }
