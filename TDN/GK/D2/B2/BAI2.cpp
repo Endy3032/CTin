66f2ff128423524097014c62504dc305501603cc
Endy3032 <61771609+Endy3032@users.noreply.github.com>
2022-11-01 19:25:14 +0700

    add midterm

---
+++ TDN/GK/D2/B2/BAI2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll n, m;
+set<ll> c;
+vector<ll> t(1e6 + 1);
+
+int main() {
+    freopen("BAI2.INP", "r", stdin);
+    freopen("BAI2.out", "w", stdout);
+
+    // Nhập số & phân loại số bằng set
+    cin >> n;
+    for (ll i = 0; i < n; i++) {
+        cin >> m;
+        c.insert(m);
+        t[m]++;
+    }
+
+    // Test từng số trong set
+    m = 0;
+    for (auto x: c) {
+        n = t[x];
+        m += n / x;
+        if (n % x != 0) m++;
+    }
+    cout << m << endl;
+}
