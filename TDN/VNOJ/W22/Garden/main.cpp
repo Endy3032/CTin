a6b010055b873f766aa255a658969f663a2e40b2
Endy3032 <endy@endy3032.ml>
2023-02-19 22:22:51 +0700

    w22 garden

--- TDN/VNOJ/W22/Garden/main.cpp
+++ TDN/VNOJ/W22/Garden/main.cpp
@@ -2,37 +2,32 @@
 using namespace std;
 
 typedef long long ll;
-ll t, n, k, a, b, d;
+ll t, n, k, d, o;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	cin >> t;
+
 	while (t--) {
 		cin >> n >> k;
 
 		vector<ll> ns(k);
 		for (int i = 0; i < k; i++) cin >> ns[i];
 
-		// cout << n << " " << k << endl;
-		// for (auto i: ns) cout << i << " ";
-		// cout << "\n\n";
-
 		sort(ns.begin(), ns.end());
-		if (ns[k - 1] != n) ns.push_back(n + 1);
+		ns.insert(ns.begin(), 0);
+		ns.push_back(n + 1);
 
-		d = 0;
-		a = ns[0] == 1;
+		o = 0;
 
-		for (int i = 0; i < ns.size(); i++) {
-			b = ns[i];
-			ll aaa = (i != 0 && ns[i] != n + 1 ? abs(b - a) / 2 + 1 : abs(b - a));
-			d = max(d, aaa);
-			// cout << a << " " << b << " - " << aaa << endl;
-			swap(a, b);
+		for (int i = 0; i < ns.size() - 1; i++) {
+			d = ns[i + 1] - ns[i] + 1;
+			if (i == 0 || i == ns.size() - 2) o = max(o, d - 1);
+			else if (d % 2 != 0) o = max(o, d / 2 + 1);
+			else o = max(o, d / 2);
 		}
-		// cout << d << "\n-----\n";
-		cout << d << endl;
+		cout << o << endl;
 	}
 }
